d354b3d52e59ddd826c7be559af616923acfd06a
Elizabeth Yu <elizabeth.yu@auspost.com.au>
2022-01-10 15:12:21 +1100

    updgrade parcel version to parcel 2

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return <div id="home">
-    <div className="typing-animation">Hello \(^_^)/</div>
+    <div className="typing-animation">Hello \(^_^)/ :D</div>
   </div>;
 };
 
